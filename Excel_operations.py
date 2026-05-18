55f90dfd110514a88eab28806d1f01607e4e887c
Shankar Bathula <41106397+sankar-bathula@users.noreply.github.com>
2026-05-18 22:37:48 +0530

    Add files via upload
    Perform Excel Operations

---
+++ Excel_operations.py
@@ -0,0 +1,48 @@
+import pandas as pd
+from openpyxl import load_workbook
+from datetime import datetime
+
+# Output Excel file
+output_file = "D://Dev_GoogleAntigravity//scripts//Data//outputs//output_report.xlsx"
+
+
+# Excel file path
+file_path = "D://Dev_GoogleAntigravity//scripts//Data//inputs//CashForecast 11-MAY-2026.xlsx"
+
+# Load workbook and sheet
+book = load_workbook(file_path)
+writer = pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='overlay')
+
+writer.book = book
+
+# Select sheet
+sheet = book["Sap Extract"]
+
+# Write today's date into AC1
+sheet["AC1"] = datetime.today().strftime("%m/%d/%Y")
+
+# Save file
+book.save(file_path)
+
+print("Date written successfully in AC1")
+
+# # Select required columns
+# selected_columns = [
+#     "Invoice Reference",
+#     "Amount in Local Currency",
+#     "Delivery date"
+# ]
+
+# df_selected = df[selected_columns]
+
+# Paste as values (write only data)
+# f_selected.to_excel(output_file, index=False)
+# Read entire sheet
+df = pd.read_excel(file_path, sheet_name="Sap Extract")
+
+# Copy and paste as values into new Excel file
+df.to_excel(output_file, sheet_name="Sap Extract", index=False)
+
+print("Entire sheet copied as values successfully.")
+
+print(f"Output File: {output_file}")
